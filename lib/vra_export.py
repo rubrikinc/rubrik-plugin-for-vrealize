e45b261db0452321c3e67ad52ad8a073afdda3b4
Peter J. Milanese <peterm@milanese.life>
2018-06-11 11:13:44 -0400

    update

--- lib/vra_export.py
+++ lib/vra_export.py
@@ -123,7 +123,7 @@ class VRASession:
 
     def download_vro(self):
         #output = session.get_call("/vco/api/packages/com.rubrik.devops?exportConfigurationAttributeValues=false&exportGlobalTags=false&exportVersionHistory=true&exportAsZip=true&exportConfigSecureStringAttributeValues=false")
-        session.get_call_download("/vco/api/packages/com.rubrik.devops?exportConfigurationAttributeValues=false&exportGlobalTags=false&exportVersionHistory=true&exportAsZip=true&exportConfigSecureStringAttributeValues=false", output['name']+".zip")
+        session.get_call_download("/vco/api/packages/com.rubrik.devops?exportConfigurationAttributeValues=false&exportGlobalTags=false&exportVersionHistory=true&exportAsZip=true&exportConfigSecureStringAttributeValues=false", "tmp.zip")
 
 if __name__ == '__main__':
     session = VRASession(sys.argv[1:])
